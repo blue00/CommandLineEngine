d3ecffec0c50f2505a712b7fc09898344abb8734
blue00 <tukding2@gmail.com>
2016-10-25 00:19:15 +0200

    Fixed some Casts Moved code from screen to mathf

--- CommandLineEngine/mathf.cpp
+++ CommandLineEngine/mathf.cpp
@@ -1,10 +1,11 @@
 #include "mathf.h"
+#include <limits> // for infinity
 
 namespace Mathf
 {
 	std::vector<Point> Raytrace(int x0, int y0, int x1, int y1)
 	{
-		std::vector<Mathf::Point> points;
+		std::vector<Point> points;
 
 		int dx = abs(x1 - x0);
 		int dy = abs(y1 - y0);
@@ -19,7 +20,7 @@ namespace Mathf
 
 		for (; n > 0; --n)
 		{
-			Mathf::Point newPoint;
+			Point newPoint;
 			newPoint.x = x;
 			newPoint.y = y;
 			points.push_back(newPoint);
@@ -33,6 +34,78 @@ namespace Mathf
 			{
 				y += y_inc;
 				error += dx;
+			}
+		}
+
+		return points;
+	}
+
+	std::vector<Point> Raytrace(float x0, float y0, float x1, float y1)
+	{
+		std::vector<Point> points;
+
+		float dx = fabs(x1 - x0);
+		float dy = fabs(y1 - y0);
+
+		int x = int(floor(x0));
+		int y = int(floor(y0));
+
+		int n = 1;
+		int x_inc, y_inc;
+		float error;
+
+		if (dx == 0)
+		{
+			x_inc = 0;
+			error = std::numeric_limits<double>::infinity();
+		}
+		else if (x1 > x0)
+		{
+			x_inc = 1;
+			n += int(floor(x1)) - x;
+			error = (floor(x0) + 1 - x0) * dy;
+		}
+		else
+		{
+			x_inc = -1;
+			n += x - int(floor(x1));
+			error = (x0 - floor(x0)) * dy;
+		}
+
+		if (dy == 0)
+		{
+			y_inc = 0;
+			error -= std::numeric_limits<float>::infinity();
+		}
+		else if (y1 > y0)
+		{
+			y_inc = 1;
+			n += int(floor(y1)) - y;
+			error -= (floor(y0) + 1 - y0) * dx;
+		}
+		else
+		{
+			y_inc = -1;
+			n += y - int(floor(y1));
+			error -= (y0 - floor(y0)) * dx;
+		}
+
+		for (; n > 0; --n)
+		{
+			Point newPoint;
+			newPoint.x = x;
+			newPoint.y = y;
+			points.push_back(newPoint);
+
+			if (error > 0)
+			{
+				y += y_inc;
+				error -= dx;
+			}
+			else
+			{
+				x += x_inc;
+				error += dy;
 			}
 		}
 
